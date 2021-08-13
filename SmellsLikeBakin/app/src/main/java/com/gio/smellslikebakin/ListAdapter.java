d0ead8ea550745b9b5c412f4f11881a871659cd1
Giovanni Salas <84675680+Gioskywalker97@users.noreply.github.com>
2021-08-13 09:52:50 -0400

    Revert "Speed app journey"

---
+++ SmellsLikeBakin/app/src/main/java/com/gio/smellslikebakin/ListAdapter.java
@@ -0,0 +1,21 @@
+package com.gio.smellslikebakin;
+
+public class ListAdapter extends RecyclerAdapter {
+
+
+    private final ListFragment.OnRecipeSelectedInterface mListener;
+
+    public ListAdapter(ListFragment.OnRecipeSelectedInterface listener) {
+        mListener = listener;
+    }
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.list_item;
+    }
+
+    @Override
+    protected void onRecipeSelected(int index) {
+        mListener.onListRecipeSelected(index);
+    }
+}
